ae513966f8749a5881fdd1f4371054f0cf74361d
tripleAVB <venturebuildertriplea@gmail.com>
2023-03-02 19:07:16 -0300

    Designs implementados em todas as paginas

--- src/app/pages/trabalheconosco/trabalheconosco.page.ts
+++ src/app/pages/trabalheconosco/trabalheconosco.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-trabalheconosco',
@@ -7,9 +9,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrabalheconoscoPage implements OnInit {
 
-  constructor() { }
+  selectedOption: string = '';
 
-  ngOnInit() {
+  showMenu: boolean = false;
+
+  toggleMenu(): void {
+    this.showMenu = !this.showMenu;
   }
+ 
+  constructor(private router: Router) {}
+
+  goToPage(option: string) {
+    switch (option) {
+      case 'option1':
+        this.router.navigateByUrl('/option1');
+        break;
+      case 'option2':
+        this.router.navigateByUrl('/option2');
+        break;
+      case 'option3':
+        this.router.navigateByUrl('/option3');
+        break;
+      default:
+        break;
+    }
+
+  }
+
+openExternalLinkFacebook(){
+  window.open('https://www.facebook.com', '_blank')
+}
+
+openExternalLinkInstagram(){
+  window.open('https://www.instagram.com', '_blank')
+}
+
+openExternalLinkYouTube(){
+  window.open('https://www.youtube.com', '_blank')
+}
+
+ngOnInit(): void{
+
+}
+
 
 }
