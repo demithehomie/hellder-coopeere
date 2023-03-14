8c7e5dc25710097b20253d94b4eb2635807b1083
tripleAVB <venturebuildertriplea@gmail.com>
2023-03-14 16:47:27 -0300

    Produzindo Carrosel da Galeria ...

--- src/app/pages/galeria/galeria.page.ts
+++ src/app/pages/galeria/galeria.page.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import { IonSlides } from '@ionic/angular';
+import { IonSlides, ModalController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-galeria',
@@ -9,6 +10,94 @@ import { IonSlides } from '@ionic/angular';
 })
 export class GaleriaPage implements OnInit {
 
+  currentIndex: number = 0;
+
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400,
+    showButtons: true
+  };
+  
+
+  images = [
+     {src: "https://www.coopeere.eco.br/wp-content/uploads/2020/10/IMG-20191119-WA0175-300x225.jpg"},
+    { src: "https://www.coopeere.eco.br/wp-content/uploads/2020/10/IMG-20181203-WA0128-300x225.jpg"},
+  {  src: "https://www.coopeere.eco.br/wp-content/uploads/2020/10/IMG-20200627-WA0783-300x225.jpg"},
+ {  src:  "https://www.coopeere.eco.br/wp-content/uploads/2020/10/IMG-20171123-WA0001-300x169.jpg"},
+    { src: "https://www.coopeere.eco.br/wp-content/uploads/2020/10/IMG-20171021-WA0002-300x225.jpg"},
+     { src:"https://www.coopeere.eco.br/wp-content/uploads/2020/10/IMG-20171023-WA0024-300x169.jpg"},
+   {  src: "https://www.coopeere.eco.br/wp-content/uploads/2020/10/FB_IMG_1522538580786-300x225.jpg"},
+     {src:"https://www.coopeere.eco.br/wp-content/uploads/2020/10/IMG-20181203-WA0093-300x225.jpg"},
+    { src:"https://www.coopeere.eco.br/wp-content/uploads/2020/10/IMG-20180505-WA0001-300x225.jpg"},
+  {  src: "https://www.coopeere.eco.br/wp-content/uploads/2020/10/IMG-20171128-WA0003-300x225.jpg"},
+{  src:   "https://www.coopeere.eco.br/wp-content/uploads/2020/10/IMG-20171009-WA0004-300x225.jpg"},
+    { src:"https://www.coopeere.eco.br/wp-content/uploads/2020/10/IMG-20171009-WA0005-300x225.jpg"},
+    { src:"https://www.coopeere.eco.br/wp-content/uploads/2020/10/IMG-20171010-WA0022-300x225.jpg"},
+{   src:  "https://www.coopeere.eco.br/wp-content/uploads/2020/10/IMG-20171128-WA0006-225x300.jpg"},
+   { src: "https://www.coopeere.eco.br/wp-content/uploads/2020/10/IMG-20191109-WA0306-300x225.jpg"},
+   {src:  "https://www.coopeere.eco.br/wp-content/uploads/2020/10/IMG-20190318-WA0065-300x225.jpg"},
+{   src:  "https://www.coopeere.eco.br/wp-content/uploads/2020/10/IMG-20171113-WA0015-300x225.jpg"},
+   { src: "https://www.coopeere.eco.br/wp-content/uploads/2020/10/IMG-20171010-WA0026-300x225.jpg"},
+ {  src:  "https://www.coopeere.eco.br/wp-content/uploads/2020/10/IMG-20191109-WA0302-300x225.jpg"},
+  { src:  "https://www.coopeere.eco.br/wp-content/uploads/2020/10/IMG-20190722-WA0061-300x225.jpg"},
+   {src:  "https://www.coopeere.eco.br/wp-content/uploads/2020/10/FB_IMG_1512173979285-300x225.jpg"},
+    {src: "https://www.coopeere.eco.br/wp-content/uploads/2020/10/20190625_110413-300x169.jpg"},
+   { src: "https://www.coopeere.eco.br/wp-content/uploads/2020/10/IMG-20180608-WA0006-300x225.jpg"},
+   { src: "https://www.coopeere.eco.br/wp-content/uploads/2020/10/IMG-20171127-WA0054-300x225.jpg"},
+    {src: "https://www.coopeere.eco.br/wp-content/uploads/2020/10/FB_IMG_1512173991571-300x225.jpg"},
+   { src: "https://www.coopeere.eco.br/wp-content/uploads/2020/10/IMG-20181204-WA0070-169x300.jpg"},
+{   src:  "https://www.coopeere.eco.br/wp-content/uploads/2020/10/IMG-20181204-WA0072-300x169.jpg"},
+  { src:  "https://www.coopeere.eco.br/wp-content/uploads/2020/10/IMG-20191125-WA0331-300x225.jpg"},
+  { src:  "https://www.coopeere.eco.br/wp-content/uploads/2020/10/IMG-20171019-WA0008-300x225.jpg"},
+  { src:  "https://www.coopeere.eco.br/wp-content/uploads/2020/10/20180613_212302-300x169.jpg"},
+ {  src:  "https://www.coopeere.eco.br/wp-content/uploads/2020/10/received_1624205220934072-300x225.jpg"},
+{  src:   "https://www.coopeere.eco.br/wp-content/uploads/2020/10/20171127_104608-300x180.jpg"},
+{   src:  "https://www.coopeere.eco.br/wp-content/uploads/2020/10/IMG-20171127-WA0004-300x180.jpg"},
+ { src:   "https://www.coopeere.eco.br/wp-content/uploads/2020/10/IMG-20181203-WA0046-225x300.jpg"},
+    { src:"https://www.coopeere.eco.br/wp-content/uploads/2020/10/IMG-20190722-WA0073-300x225.jpg"},
+{   src:  "https://www.coopeere.eco.br/wp-content/uploads/2020/10/20181018_160939-169x300.jpg"},
+{  src:   "https://www.coopeere.eco.br/wp-content/uploads/2020/10/IMG-20190626-WA0043-225x300.jpg"},
+  { src:  "https://www.coopeere.eco.br/wp-content/uploads/2020/10/20171019_145015-300x180.jpg"},
+     {src:"https://www.coopeere.eco.br/wp-content/uploads/2020/10/20171122_155600-180x300.jpg"},
+   { src: "https://www.coopeere.eco.br/wp-content/uploads/2020/10/IMG-20171128-WA0005-225x300.jpg"},
+    {src: "https://www.coopeere.eco.br/wp-content/uploads/2020/10/IMG-20171111-WA0001-300x199.jpg"},
+    { src:"https://www.coopeere.eco.br/wp-content/uploads/2020/10/20181018_170201-300x169.jpg"},
+     {src:"https://www.coopeere.eco.br/wp-content/uploads/2020/10/IMG-20200716-WA1018-300x300.jpg"},
+{  src:   "https://www.coopeere.eco.br/wp-content/uploads/2020/10/Screenshot_20180411-085402-169x300.jpg"},
+  ]
+  selectedImage: any;
+  image: any;
+  mostrarCarousel: boolean = false;
+
+
+  showCarousel() {
+    this.selectedImage = this.image;
+    this.mostrarCarousel = !this.mostrarCarousel;
+  }
+
+  openCarousel = () =>{
+    this.mostrarCarousel = true;
+  }
+  
+
+  closeCarousel() {
+    this.mostrarCarousel = false;
+  }
+  
+  nextSlide() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+  
+  prevSlide() {
+    this.currentIndex = (this.currentIndex + this.images.length - 1) % this.images.length;
+  }
+  
+ handleImageClick(index: number) {
+    this.currentIndex = index;
+    this.mostrarCarousel = true;
+  }
+  
+currentSlide = 0;
   pesquisaPagina: any;
 
   filtros: any;
@@ -30,15 +119,7 @@ enviarFormulario() {
   this.router.navigate(['/resultados'], { queryParams: this.filtros });
 }
 
-  @ViewChild('slides')
-  slides!: IonSlides;
-  slideOpts = {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    centeredSlides: true,
-    allowTouchMove: false
-  };
-  currentIndex = 0;
+
   
 
 
@@ -53,8 +134,20 @@ enviarFormulario() {
 
   selectedOption: string = '';
  
+  toggleFullScreen() {
+    let elem = document.documentElement;
+  
+    if (!document.fullscreenElement) {
+      elem.requestFullscreen();
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      }
+    }
+  }
+  
  
-  constructor(private router: Router) {}
+  constructor(private router: Router, private modalCtrl: ModalController) {}
 
   goToPage(option: string) {
     switch (option) {
@@ -92,19 +185,10 @@ ngOnInit(): void{
 
 
 
-ionViewDidEnter() {
-  this.slides.ionSlideDidChange.subscribe(async () => {
-    this.currentIndex = await this.slides.getActiveIndex();
-  });
-}
 
-async onScroll() {
-  if (this.currentIndex === this.slidesArray.length - 1) {
-    this.currentIndex = 0;
-  } else {
-    this.currentIndex++;
-  }
 
-  await this.slides.slideTo(this.currentIndex);
-}
+
+
+
+
 }
