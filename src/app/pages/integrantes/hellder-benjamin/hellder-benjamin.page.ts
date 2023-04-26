35b04bd11cf0473f18ea359d1627875712eefcd7
Demetrius Ferreira <demithehomie@gmail.com>
2023-04-26 17:03:17 -0300

    Adição do modelo de integrantes - Hellder adicionado - falta aprimorar responsividade

--- src/app/pages/integrantes/hellder-benjamin/hellder-benjamin.page.ts
+++ src/app/pages/integrantes/hellder-benjamin/hellder-benjamin.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-hellder-benjamin',
@@ -6,10 +8,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hellder-benjamin.page.scss'],
 })
 export class HellderBenjaminPage implements OnInit {
+  
+  
+  selectedOption: string;
 
-  constructor() { }
+  showMenu: boolean = false;
 
-  ngOnInit() {
+  toggleMenu(): void {
+    this.showMenu = !this.showMenu;
   }
+  
+  constructor(private navCtrl: NavController, private titleService: Title) {
+    this.selectedOption = 'quemsomos';
+    this.selectedOption = 'objetivos';
+    this.selectedOption = 'equipe';
+    this.titleService.setTitle('Hellder Benjamin')
+  }
+
+
+  onOptionSelected(option: string) {
+    this.selectedOption = option;
+    switch (option) {
+      case 'quemsomos':
+        this.navCtrl.navigateForward('/quemsomos');
+        break;
+      case 'objetivos':
+        this.navCtrl.navigateForward('/objetivos');
+        break;
+      case 'equipe':
+        this.navCtrl.navigateForward('/equipe');
+          break;  
+    }
+
+}
+
+
+
+openExternalLinkFacebook(){
+  window.open('https://www.facebook.com', '_blank')
+}
+
+openExternalLinkInstagram(){
+  window.open('https://www.instagram.com', '_blank')
+}
+
+openExternalLinkYouTube(){
+  window.open('https://www.youtube.com', '_blank')
+}
+
+ngOnInit(): void{
+
+}
+
 
 }
