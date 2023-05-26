07f7a3a3b43ccc002f70e6835701b546812dde20
demithehomie <demithehomie@gmail.com>
2023-05-26 20:50:50 -0300

    CRUD completo e autenticação para administradores

--- src/app/pages/admin-login/admin-login.module.ts
+++ src/app/pages/admin-login/admin-login.module.ts
@@ -1,20 +1,27 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import {  CommonModule} from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AdminLoginPageRoutingModule } from './admin-login-routing.module';
 
 import { AdminLoginPage } from './admin-login.page';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { AppStorageService } from 'src/app/services/app-storage.service';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @NgModule({
   imports: [
+    HttpClientModule,
+    ReactiveFormsModule,
     CommonModule,
     FormsModule,
     IonicModule,
     AdminLoginPageRoutingModule
   ],
-  declarations: [AdminLoginPage]
+  declarations: [AdminLoginPage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  providers: [AuthenticationService, Storage, AppStorageService, UsuarioService]
 })
 export class AdminLoginPageModule {}
--- src/app/pages/admin-login/admin-login.page.ts
+++ src/app/pages/admin-login/admin-login.page.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
-import { AlertController, LoadingController } from '@ionic/angular';
+import { AlertController, LoadingController, NavController } from '@ionic/angular';
+import { Login } from 'src/app/interfaces/login';
+import { AppStorageService } from 'src/app/services/app-storage.service';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
@@ -12,8 +15,14 @@ import { UsuarioService } from 'src/app/services/usuario.service';
 })
 export class AdminLoginPage implements OnInit {
 
-  email!: string;
-  password!: string;
+  credentials!: FormGroup;
+  showMenu: boolean = false;
+
+  toggleMenu(): void {
+    this.showMenu = !this.showMenu;
+  }
+
+  
 
   minutos!: number;
   segundos!: number;
@@ -29,74 +38,167 @@ export class AdminLoginPage implements OnInit {
   constructor(
     private router: Router,
     private formBuilder: FormBuilder,
+    private usersService: UsuarioService,
+    public navCtrl: NavController,
     private alertController: AlertController,
     private loadingController: LoadingController,
-    private usuarioService: UsuarioService,
-    private title: Title
+    private authenticationService: AuthenticationService,
+    private titleController: Title,
+    private appStorageService: AppStorageService
     
     ) { 
 
-      this.title.setTitle('ADMIN')
+      this.titleController.setTitle('ADMIN')
       this.minutos = 5;
       this.segundos = 0;
     }
 
 
-  login() {
-    if (this.email === 'helrov@hotmail.com' && this.password === 'tara9317') {
-      // Autenticação bem-sucedida, redirecionar para a página principal
-      this.router.navigate(['/admin-dashboard']);
-    } else {
-      // Exibir mensagem de erro de autenticação inválida
-      console.log('Credenciais inválidas');
-      this.errorMessage = 'Acesso Negado! Verifique suas credenciais.';
-      this.successMessage = '';
+    login: Login = {
+      email: "",
+      password: ""
+    };
+  
+    ngOnInit(): void {
+      this.validaForm();
     }
+  
+    validaForm() {
+      this.credentials = this.formBuilder.group({
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]]
+      });
+    }
+
+     //// STORAGE - BELOW
+
+  setToken(token: any){
+    this.appStorageService.set(`token`,`${token}`)
   }
 
+  setId(id: any){
+    this.appStorageService.set(`id`, `${id}`)
+  }
+  
+  setUsername(name: any){
+    this.appStorageService.set(`name`,`${name}`)
+  }
+  
+  setEmail(email: any){
+    this.appStorageService.set(`email`,`${email}`)
+  }
+  
+  setCPF(cpfCnpj: any){
+    this.appStorageService.set(`cpfCnpj`,`${cpfCnpj}`)
+  }
+  
+  setMobilePhone(mobilePhone: any){
+    this.appStorageService.set(`mobilePhone`,`${mobilePhone}`)
+  }
+  
+  setPhone(phone: any){
+    this.appStorageService.set(`phone`,`${phone}`)
+  }
+  
+  setCompany(company: any){
+    this.appStorageService.set(`company`,`${company}`)
+  }
+  
+  setPostalCode(postalCode: any){
+    this.appStorageService.set(`postalCode`,`${postalCode}`)
+  }
+  
+  setAddress(address: any){
+    this.appStorageService.set(`address`,`${address}`)
+  }
+  
+  setState(state: any){
+    this.appStorageService.set(`state`,`${state}`)
+  }
 
-
+  setProvince(province: any){
+    this.appStorageService.set(`province`,`${province}`)
+  }
+  
+  setCity(city: any){
+    this.appStorageService.set(`city`,`${city}`)
+  }
+  
+  setAddressNumber(addressNumber: any){
+    this.appStorageService.set(`addressNumber`,`${addressNumber}`)
+  }
+  
   
 
-  ngOnInit(): void {
-    this.data = this.formBuilder.group({
-      verificationCode: ["", 
-        [Validators.required, 
-          Validators.minLength(1), 
-            Validators.maxLength(5),
-              
-          ]],
-    });
-  }
+///// STORAGE - ABOVE (FUNCTIONS IMPLEMENTED BELOW)
 
+  
+    async onSubmit() {
+      const loading = await this.loadingController.create();
+      await loading.present();
+  
+      this.authenticationService.loginSimplificado(this.credentials.value).subscribe(
+        async (res: any) => {
+          if (res.role === 2) {
+            this.router.navigateByUrl('/admin-dashboard', { replaceUrl: true });
+            this.setToken(res.token.accessToken);
+            this.setId(res.id);
+            this.setUsername(res.name);
+            this.setEmail(res.email);
+            this.setCPF(res.cpfCnpj);
+            this.setMobilePhone(res.mobilePhone);
+            this.setPhone(res.phone);
+            this.setCompany(res.company);
+            this.setPostalCode(res.postalCode);
+            this.setProvince(res.province);
+            this.setAddress(res.address);
+            this.setAddressNumber(res.addressNumber);
+            this.setState(res.state);
+            this.setCity(res.city);
+  
+            console.log(res);
+            await loading.dismiss();
+            this.presentSuccessAlert();
+          } else {
+            await loading.dismiss();
+            this.presentErrorAlert('Acesso não autorizado.');
+          }
+        },
+        async () => {
+          await loading.dismiss();
+          this.presentErrorAlert('Seu login não pôde ser autenticado.');
+        }
+      );
+    }
+  
+    async presentSuccessAlert() {
+     // const res = this.onSubmit.r
+      const alert = await this.alertController.create({
+        // header: `Bem-Vindo, ${res.name}`,
+        header: `Bem-Vindo a Área de Membros`,
+        message: `Suas credenciais foram validadas`,
+        buttons: ['OK']
+      });
+  
+      await alert.present();
+    }
+  
+    async presentErrorAlert(message: string) {
+      const alert = await this.alertController.create({
+        header: 'Falha na autenticação',
+        message: message,
+        buttons: ['OK']
+      });
+    
+      await alert.present();
+    }
+    
 
-
-async forward(){
-  this.router.navigateByUrl('/login')
-}
-
-body!: any
-
-async resend(id: number){
-this.usuarioService.startConfirmSMS(id)
-console.log(id)
-}
-
-
-
-ionViewDidEnter() {
-
-}
-
-ionViewWillLeave() {
-
-}
-
-
-
-executeBoth(){
-
-this.resend(this.id)
-}
-
+    get email() {
+      return this.credentials.get('email');
+    }
+    
+    get password() {
+      return this.credentials.get('password');
+    }
 }
